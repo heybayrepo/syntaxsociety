bc56f40a8e60dda87dbd1aaae0e16b39e4183847
bayu <bayumaitra222@gmail.com>
2025-11-23 11:24:55 +0700

    add scripts folder for streamlit

---
+++ scripts/streamlit_training.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+st.title("The Promotion Paradox")
+st.header('Fixing Biased Talent Promotion Decisions Through Data-Driven HR Analytics')
+st.write("A project by Syntax Society")    
+
